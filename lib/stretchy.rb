91ed9ea4fafef5db2814d6a13b491088b69544b8
Spencer Markowski <spencer@theablefew.com>
2024-03-02 17:07:13 -0500

    Register attribute types

--- lib/stretchy.rb
+++ lib/stretchy.rb
@@ -2,14 +2,21 @@
 require 'zeitwerk'
 require 'amazing_print'
 require 'rainbow'
-require 'elasticsearch/rails'
+require 'jbuilder'
+# require 'elasticsearch/rails'
+require 'elasticsearch/model'
 require 'elasticsearch/persistence'
-require 'active_support/concern' 
-require 'active_support/core_ext'
+# require 'active_support/concern' 
+require 'active_model'
+require 'active_support/all'
+require 'active_model/type/array'
+require 'active_model/type/hash'
+
+ActiveModel::Type.register(:array, ActiveModel::Type::Array)
+ActiveModel::Type.register(:hash, ActiveModel::Type::Hash)
 
 require_relative "stretchy/version"
-require_relative "stretchy/instrumentation"
-# require_relative "stretchy/associations"
+require_relative "stretchy/instrumentation/railtie" if defined?(Rails)
 
 module Stretchy
 
