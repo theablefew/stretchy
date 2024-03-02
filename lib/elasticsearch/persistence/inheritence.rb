9d07df7614b5996ee15f1fa79cf48f09c0bda17a
Spencer Markowski <spencer@theablefew.com>
2024-03-02 17:06:02 -0500

    Remove monkey patch

--- lib/elasticsearch/persistence/inheritence.rb
+++
@@ -1,9 +0,0 @@
-module Elasticsearch
-  module Persistence
-    module Inheritence
-      def base_class
-        self
-      end
-    end
-  end
-end
--- lib/elasticsearch/persistence/model/errors.rb
+++
@@ -1,9 +0,0 @@
-module Elasticsearch
-  module Persistence
-    module Model
-      class DocumentNotSaved     < StandardError; end
-      class DocumentNotPersisted < StandardError; end
-      class QueryOptionMissing < StandardError; end
-    end
-  end
-end
