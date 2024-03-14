dce954acde965eb7e418b7559319381a74db6ab1
Spencer Markowski <spencer@theablefew.com>
2024-03-14 08:21:07 -0400

    Fix default_size

--- lib/stretchy/common.rb
+++ lib/stretchy/common.rb
@@ -15,8 +15,9 @@ module Stretchy
                 @default_sort_key
             end
 
-            def default_size(size = 10000)
-                @default_size = size
+            def default_size(size = nil)
+                @default_size = size unless size.nil?
+                @default_size
             end
 
             private
