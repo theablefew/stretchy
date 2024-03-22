e768ee327be94ecd55dedb667861da9239203427
Spencer Markowski <spencer@theablefew.com>
2024-03-22 11:20:33 -0400

    Update documentation

--- lib/stretchy/model/common.rb
+++ lib/stretchy/model/common.rb
@@ -3,24 +3,141 @@ module Stretchy
         module Common
             extend ActiveSupport::Concern
 
+            # Get the highlighted results for a specific field.
+            #
+            # This method is used to get the highlighted results for a specific field from the search results. It accepts a field for which to get the highlighted results.
+            #
+            # ### Parameters
+            #
+            # - `field:` The Symbol or String representing the field for which to get the highlighted results.
+            #
+            # ### Returns
+            # Returns an Array of Strings representing the highlighted results for the specified field.
+            #
+            # ---
+            #
+            # ### Examples
+            #
+            # #### Get the highlighted results for a field
+            #
+            # ```ruby
+            #   result = Model.query_string("name: Soph*").highlight(name: {pre_tags: "__", post_tags: "__"}).first
+            #   result.highlights_for(:name)
+            # ```
+            #
             def highlights_for(attribute)
                 highlights[attribute.to_s]
             end
 
             class_methods do
 
-                # Set the default sort key to be used in sort operations
+                # Set or get the default sort key.
+                #
+                # This method is used to set or get the default sort key to be used in sort operations. If a field is provided, it sets the field as the default sort key. If no field is provided, it returns the current default sort key.
+                #
+                # `created_at` is the default sort key.
+                #
+                # ### Parameters
+                #
+                # - `field:` The Symbol or String representing the field to be used as the default sort key (optional).
+                #
+                # ### Returns
+                # Returns the current default sort key if no field is provided. If a field is provided, it sets the field as the default sort key and returns the field.
+                #
+                # ---
+                #
+                # ### Examples
+                #
+                # #### Set the default sort key
+                #
+                # ```ruby
+                #   class Model < StretchyModel
+                #       default_sort_key :updated_at
+                #   end
+                # ```
+                #
+                # #### Get the default sort key
+                #
+                # ```ruby
+                #   Model.default_sort_key
+                # ```
                 #
                 def default_sort_key(field = nil)
                     @default_sort_key = field unless field.nil?
                     @default_sort_key
                 end
 
+
+                # Set or get the default size.
+                #
+                # This method is used to set or get the default size to be used in operations that return documents. If a size is provided, it sets the size as the default size. 
+                # If no size is provided, it returns the current default size.
+                # 
+                # 10000 is the default size for StretchyModel
+                #
+                # ### Parameters
+                #
+                # - `size:` The Integer representing the size to be used as the default size (optional).
+                #
+                # ### Returns
+                # Returns the current default size if no size is provided. If a size is provided, it sets the size as the default size and returns the size.
+                #
+                # ---
+                #
+                # ### Examples
+                #
+                # #### Set the default size
+                #
+                # ```ruby
+                #   class Model < StretchyModel
+                #       default_size 100
+                #   end
+                # ```
+                #
+                # #### Get the default size
+                #
+                # ```ruby
+                #   Model.default_size
+                # ```
+                #
                 def default_size(size = nil)
                     @default_size = size unless size.nil?
                     @default_size
                 end
 
+                
+                # Set or get the default pipeline.
+                #
+                # This method is used to set or get the default pipeline at the index level to be used in operations that require a pipeline. 
+                #  
+                # If a pipeline is provided, it sets the pipeline as the default pipeline. 
+                # If no pipeline is provided, it returns the current default pipeline.
+                #
+                # ### Parameters
+                #
+                # - `pipeline:` The String representing the pipeline to be used as the default pipeline (optional).
+                #
+                # ### Returns
+                # Returns the current default pipeline if no pipeline is provided. If a pipeline is provided, it sets the pipeline as the default pipeline and returns the pipeline.
+                #
+                # ---
+                #
+                # ### Examples
+                #
+                # #### Set the default pipeline
+                #
+                # ```ruby
+                #   class Model < StretchyModel
+                #       default_pipeline 'my_pipeline'
+                #   end
+                # ```
+                #
+                # #### Get the default pipeline
+                #
+                # ```ruby
+                #   Model.default_pipeline
+                # ```
+                #
                 def default_pipeline(pipeline = nil)
                     @default_pipeline = pipeline.to_s unless pipeline.nil?
                     @default_pipeline
--- lib/stretchy/relation.rb
+++ lib/stretchy/relation.rb
@@ -15,7 +15,6 @@ module Stretchy
                 Relations::SearchOptionMethods, 
                 Delegation
 
-        # Getters.
         attr_reader :klass, :loaded
         alias :model :klass
         alias :loaded? :loaded
@@ -23,10 +22,28 @@ module Stretchy
         # Delegates to the results array.
         delegate :blank?, :empty?, :any?, :many?, to: :results
 
-        # Constructor.
+        # Initialize a new instance of the relation.
         #
-        # @param klass [Class] The class of the Elasticsearch documents.
-        # @param values [Hash] The initial values for the relation.
+        # This constructor method is used to create a new instance of the relation. It accepts a class representing the Elasticsearch documents and a hash of initial values for the relation.
+        #
+        # ### Parameters
+        #
+        # - `klass:` The Class representing the Elasticsearch documents.
+        # - `values:` The Hash representing the initial values for the relation (optional).
+        #
+        # ### Returns
+        # Returns a new instance of the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Initialize a new relation
+        #
+        # ```ruby
+        #   relation = Relation.new(Model, {name: 'John Doe', age: 30})
+        # ```
+        #
         def initialize(klass, values={})
             @klass  = klass
             @values = values.merge(default_size: klass.default_size)
@@ -34,54 +51,173 @@ module Stretchy
             @loaded = false
         end
 
-        # Builds a new Elasticsearch document.
+        # Initialize a new instance of the relation.
         #
-        # @param args [Array] The arguments to pass to the document constructor.
-        # @return [Object] The new document.
+        # This constructor method is used to create a new instance of the relation. It accepts a class representing the Elasticsearch documents and a hash of initial values for the relation.
+        #
+        # ### Parameters
+        #
+        # - `klass:` The Class representing the Elasticsearch documents.
+        # - `values:` The Hash representing the initial values for the relation (optional).
+        #
+        # ### Returns
+        # Returns a new instance of the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Initialize a new relation
+        #
+        # ```ruby
+        #   relation = Relation.new(Model, {name: 'John Doe', age: 30})
+        # ```
+        #
         def build(*args)
          @klass.new *args
         end
 
         # Returns the results of the relation as an array.
         #
-        # @return [Array] The results of the relation.
-        def to_a
+        # This instance method is used to load the results of the relation and return them as an array. It calls the `load` method to load the results and then returns the `@records` instance variable.
+        #
+        # ### Returns
+        # Returns an Array representing the results of the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the results of a relation as an array
+        #
+        # ```ruby
+        #   relation = Relation.new(Model, {name: 'John Doe', age: 30})
+        #   results = relation.results
+        # ```
+        #
+        def results
           load
           @records
         end
-        alias :results :to_a
 
+        # _alias for `results`_
+        alias :to_a :results
+
+        # Returns the raw Elasticsearch response for the relation.
+        #
+        # This instance method is used to get the raw Elasticsearch response for the relation. It calls the `results` method to load the results and then returns the `response` property of the results.
+        #
+        # ### Returns
+        # Returns a Hash representing the raw Elasticsearch response for the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the raw Elasticsearch response for a relation
+        #
+        # ```ruby
+        #   relation = Relation.new(Model, {name: 'John Doe', age: 30})
+        #   raw_response = relation.response
+        # ```
+        #
+        # #### With a model
+        # ```ruby
+        #  relation = Model.where(name: 'John Doe')
+        #  raw_response = relation.response
+        #  #=> {"took"=>3, "timed_out"=>false, "_shards"=>{"total"=>1, "successful"=>1, "skipped"=>0, "failed"=>0}, "hits"=>{"total"=>{"value"=>0, "relation"=>"eq"}, "max_score"=>nil, "hits"=>[]}}
+        # ```
+        #
         def response
           results.response
         end
 
         # Returns the results of the relation as a JSON object.
         #
-        # @param options [Hash] The options to pass to the `as_json` method.
-        # @return [Hash] The results of the relation as a JSON object.
+        # This instance method is used to get the results of the relation as a JSON object. It calls the `as_json` method on the results of the relation and passes any provided options to it.
+        #
+        # ### Parameters
+        #
+        # - `options:` The Hash representing the options to pass to the `as_json` method (optional).
+        #
+        # ### Returns
+        # Returns a Hash representing the results of the relation as a JSON object.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the results of a relation as a JSON object
+        #
+        # ```ruby
+        #   relation = Relation.new(Model, {name: 'John Doe', age: 30})
+        #   json_results = relation.as_json
+        # ```
+        #
         def as_json(options = nil)
           results.as_json(options)
         end
 
         # Returns the Elasticsearch query for the relation.
         #
-        # @return [Hash] The Elasticsearch query for the relation.
+        # This instance method is used to get the Elasticsearch query for the relation. It calls the `to_elastic` method on the `query_builder` of the relation.
+        #
+        # ### Returns
+        # Returns a Hash representing the Elasticsearch query for the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the Elasticsearch query for a relation
+        #
+        # ```ruby
+        #   Model.where(flight: 'goat').to_elastic
+        #   #=> {"query"=>{"bool"=>{"must"=>{"term"=>{"flight.keyword"=>"goat"}}}}}
+        # ```
+        #
         def to_elastic
           query_builder.to_elastic
         end
 
         # Creates a new Elasticsearch document.
         #
-        # @param args [Array] The arguments to pass to the document constructor.
-        # @param block [Proc] The block to pass to the document constructor.
-        # @return [Object] The new document.
+        # This instance method is used to create a new Elasticsearch document within the scope of the relation. It accepts a list of arguments and an optional block to pass to the document constructor.
+        #
+        # ### Parameters
+        #
+        # - `*args:` A list of arguments to pass to the document constructor.
+        # - `&block:` An optional block to pass to the document constructor.
+        #
+        # ### Returns
+        # Returns the newly created document.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Create a new document within the scope of a relation
+        #
+        # ```ruby
+        #   document = relation.create(name: 'John Doe', age: 30)
+        # ```
+        #
         def create(*args, &block)
            scoping { @klass.create!(*args, &block) }
         end
 
         # Executes a block of code within the scope of the relation.
         #
-        # @yield The block of code to execute.
+        # This instance method is used to execute a block of code within the scope of the relation. It temporarily sets the current scope of the class to this relation, executes the block, and then resets the current scope to its previous value.
+        #
+        # ### Parameters
+        #
+        # - `&block:` The block of code to execute within the scope of the relation.
+        #
+        # ### Returns
+        # Returns the result of the block execution.
+        #
+        #
         def scoping
           previous, klass.current_scope = klass.current_scope, self
           yield
@@ -91,22 +227,41 @@ module Stretchy
 
         # Loads the results of the relation.
         #
-        # @return [Relation] The relation object.
+        # This instance method is used to load the results of the relation. It calls the `exec_queries` method to execute the queries unless the results have already been loaded.
+        #
+        # ### Returns
+        # Returns the relation object itself.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Load the results of a relation
+        #
+        # ```ruby
+        #   relation = Model.where(flight: 'goat')
+        #   relation.load
+        # ```
+        #
         def load
           exec_queries unless loaded?
           self
         end
+
+        # _alias for `load`_
         alias :fetch :load
 
-        # Deletes Elasticsearch documents.
-        #
-        # @param opts [Hash] The options for the delete operation.
-        def delete(opts=nil)
+        def delete(opts=nil) #:nodoc:
         end
 
         # Executes the Elasticsearch query for the relation.
         #
-        # @return [Array] The results of the query.
+        # This instance method is used to execute the Elasticsearch query for the relation. It calls the `execute` method on the `query_builder` of the relation and stores the results in the `@records` instance variable.
+        #
+        # ### Returns
+        # Returns an Array representing the results of the query.
+        #
+        #
         def exec_queries
           # Run safety callback
           klass._circuit_breaker_callbacks.each do |cb|
@@ -129,14 +284,44 @@ module Stretchy
 
         # Returns the values of the relation as a hash.
         #
-        # @return [Hash] The values of the relation.
+        # This instance method is used to get the values of the relation as a hash. It converts the `@values` instance variable to a hash and returns it.
+        #
+        # ### Returns
+        # Returns a Hash representing the values of the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the values of a relation
+        #
+        # ```ruby
+        #   relation = Model.where(flight: 'goat')
+        #   values = relation.values
+        # ```
+        #
         def values
           Hash[@values]
         end
 
-        # Returns a string representation of the relation.
+        #  Returns a string representation of the relation.
         #
-        # @return [String] The string representation of the relation.
+        # This instance method is used to get a string representation of the relation. It includes information about the total number of results, the maximum number of results, and any aggregations present in the response.
+        #
+        # ### Returns
+        # Returns a String representing the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get a string representation of a relation
+        #
+        # ```ruby
+        #   Model.where(flight: 'goat').terms(:flights, field: :flight).inspect
+        #   #=> #<Stretchy::Relation total: 0, max: 0, aggregations: ["flights"]> 
+        # ```
+        # 
         def inspect
           begin
             entries = to_a.results.take([size_value.to_i + 1, 11].compact.min).map!(&:inspect)
@@ -156,7 +341,22 @@ module Stretchy
 
         # Returns the query builder for the relation.
         #
-        # @return [QueryBuilder] The query builder for the relation.
+        # This private instance method is used to get the query builder for the relation. It creates a new instance of `Relations::QueryBuilder` with the values of the relation and the attribute types of the class.
+        #
+        # ### Returns
+        # Returns a `Relations::QueryBuilder` representing the query builder for the relation.
+        #
+        # ---
+        #
+        # ### Examples
+        #
+        # #### Get the query builder for a relation
+        #
+        # ```ruby
+        #   relation = Model.where(flight: 'goat')
+        #   query_builder = relation.send(:query_builder)
+        # ```
+        #
         def query_builder
           Relations::QueryBuilder.new(values, klass.attribute_types)
         end
