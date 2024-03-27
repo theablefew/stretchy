7cbe487ea4445a79912806e65cb2f2ccd301a4b8
Spencer Markowski <spencer@theablefew.com>
2024-03-27 11:49:56 -0400

    Add knn_vector type

---
+++ lib/stretchy/attributes/type/knn_vector.rb
@@ -0,0 +1,47 @@
+module Stretchy::Attributes::Type
+  # The KnnVector attribute type.
+  #
+  # This class is used to define a `knn_vector` attribute for a model. 
+  # It provides support for the Elasticsearch `knn_vector` data type, 
+  # which is a type of data type that can hold vectors of float values.
+  #
+  # ### Parameters
+  #
+  # The `knn_vector` data type supports the following options:
+  # - `dimension`: The number of dimensions in the vector.
+  # - `method`: The method used for nearest neighbor search. This is a hash that can include the following keys:
+  #   - `name`: The name of the method.
+  #   - `space_type`: The type of space used for the method.
+  #   - `engine`: The engine used for the method.
+  #   - `parameters`: Any additional parameters for the method.
+  #
+  # ---
+  #
+  # ### Examples
+  #
+  #
+  # #### Define a `knn_vector` attribute
+  #
+  # ```ruby
+  #   class MyModel < StretchyModel
+  #     attribute :my_vector, :knn_vector
+  #   end
+  # ```
+  #
+  # #### Define a `knn_vector` attribute with options
+  #
+  # ```ruby
+  #   class MyModel < StretchyModel
+  #     attribute :my_vector, :knn_vector, dimension: 3, method: { name: "hnsw", space_type: "cosinesimil", engine: "nmslib", parameters: { ef_construction: 200 } }
+  #   end
+  # ```
+  #
+  class KnnVector < Stretchy::Attributes::Type::Base
+      OPTIONS = [:dimension, :method]
+
+      def type
+        :knn_vector
+      end
+
+  end
+end
